edd2116fa08f33686702f3ae7a7a3577359ae03e
Katherine Li <katherineli0514@gmail.com>
2019-01-10 14:22:52 -0800

    Setup initial project.

---
+++ MVVMPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMPractice
+//
+//  Created by Katherine Li on 1/10/19.
+//  Copyright © 2019 Katherine Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
